f8dc26245e0aa2031f4220a8843ba23b3bd53a01
chulogentogao <chulogen2gao@gmail.com>
2025-12-18 16:13:31 +0800

    update cors

--- config/cors.php
+++ config/cors.php
@@ -12,7 +12,7 @@ return [
         'http://localhost:3000',
         'http://localhost:3001',
         'http://127.0.0.1:3000',
-        'http://127.0.0.1:3001',
+        'https://frontend-appdev-nx5m.vercel.app/api',
     ],
     'allowed_origins_patterns' => [
         '/^http:\/\/localhost(?::\d+)?$/',
